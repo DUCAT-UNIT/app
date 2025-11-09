6f1b8db9f7f2950af159011adae7398ff9472ae2
LucasRodriguez <lucasrodriguezbenitez@gmail.com>
2025-11-09 12:43:30 +0100

    Increase vault card vertical padding to 28px

--- styles.js
+++ styles.js
@@ -352,8 +352,8 @@ const styles = StyleSheet.create({
     backgroundColor: COLORS.CARD_BG,
     borderRadius: 12,
     paddingHorizontal: 8,
-    paddingTop: 20,
-    paddingBottom: 20,
+    paddingTop: 28,
+    paddingBottom: 28,
     marginBottom: SCREEN_WIDTH <= 375 ? 4 : 12,
   },
   vaultContentWrapper: {
