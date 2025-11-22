3b6032b0a22c7dd819ff80f1085bc44b0821cabb
LucasRodriguez <lucasrodriguezbenitez@gmail.com>
2025-11-22 14:13:07 +0100

    Add missing close icon to ActionIcons

--- components/icons/navigation/ActionIcons.jsx
+++ components/icons/navigation/ActionIcons.jsx
@@ -47,4 +47,23 @@ export const ActionIcons = {
       />
     </Svg>
   ),
+
+  close: ({ width, height, color }) => (
+    <Svg width={width} height={height} viewBox="0 0 24 24" fill="none">
+      <Path
+        d="M18 6L6 18"
+        stroke={color}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+      />
+      <Path
+        d="M6 6L18 18"
+        stroke={color}
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth="2"
+      />
+    </Svg>
+  ),
 };
